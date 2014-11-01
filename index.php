49095c21e28df62a4442b0d8db971521001b3499
Vladimir Gavrylov <iboved@gmail.com>
2014-11-01 15:59:26 +0200

    Testing Composer autoload

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use Iboved\Model\Article;
+
+$obj = new Article();
+$obj->sayHello();
---
+++ src/Iboved/Model/Article.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Iboved\Model;
+
+
+class Article
+{
+    public function sayHello()
+    {
+        echo "Hello, my friend!";
+    }
+} 
