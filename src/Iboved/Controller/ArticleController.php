2522f8c0585ee8e0e06b16396ba9ca5567057ffa
Vladimir Gavrylov <iboved@gmail.com>
2014-11-02 17:12:24 +0200

    Added view layer

--- src/Iboved/Controller/ArticleController.php
+++ src/Iboved/Controller/ArticleController.php
@@ -2,11 +2,18 @@
 
 namespace Iboved\Controller;
 
-
 use Symfony\Component\HttpFoundation\Response;
 
 class ArticleController
 {
+    /** @var  \Twig_Environment */
+    protected $twig;
+
+    public function __construct(\Twig_Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
     /**
      * @return Response
      */
@@ -14,36 +21,40 @@ class ArticleController
     {
         return new Response('So many articles here');
     }
+
     /**
      * @param string $id
      * @return Response
      */
     public function getArticleAction($id)
     {
-        return new Response(sprintf('Get Article with "%s" id action', $id));
+        return new Response($this->twig->render('article.html.twig', ['method' => 'Get', 'articleId' => $id]));
     }
+
     /**
      * @param string $id
      * @return Response
      */
     public function putArticleAction($id)
     {
-        return new Response(sprintf('Put Article with "%s" id  action', $id));
+        return new Response($this->twig->render('article.html.twig', ['method' => 'Put', 'articleId' => $id]));
     }
+
     /**
      * @param string $id
      * @return Response
      */
     public function postArticleAction($id)
     {
-        return new Response(sprintf('Post Article with "%s" id  action', $id));
+        return new Response($this->twig->render('article.html.twig', ['method' => 'Post', 'articleId' => $id]));
     }
+
     /**
      * @param string $id
      * @return Response
      */
     public function deleteArticleAction($id)
     {
-        return new Response(sprintf('Delete Article with "%s" id  action', $id));
+        return new Response($this->twig->render('article.html.twig', ['method' => 'Delete', 'articleId' => $id]));
     }
 }
